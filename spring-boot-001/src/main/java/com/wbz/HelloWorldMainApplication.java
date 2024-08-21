84372ff5de8e027b14cf2abbd89c0bca2b3b5e68
Cypress-w <13123188259@163.com>
2024-08-21 17:20:45 +0800

    修改主类信息

--- spring-boot-001/src/main/java/com/wbz/HelloWorldMainApplication.java
+++ spring-boot-001/src/main/java/com/wbz/HelloWorldMainApplication.java
@@ -1,10 +1,10 @@
 package com.wbz;
-@springboot
+@spring
 public class HelloWorldMainApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//测试
 	}
 
 }
